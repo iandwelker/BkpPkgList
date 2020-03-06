651e039bec2eb6499fed5f0c73f7768b366e0e0d
welkeri1 <iwmanticore8@gmail.com>
2020-03-06 10:00:52 -0700

    Made Script more universal and updated README

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Backup list of packages
 
 This just gets all your installed packages from pacman (you can change it to use your own package manager), put all their names into one file, compares it against your old file of installed packages, and combines them into one file, sorted, with duplicates removed.
 I just use this with a cron job so that I keep a running list of all the packages I've every had installed. You're free to use it too, no credit necessary cause it's so simple and I'm sure a million other people have made basically this same script.
+You'll need to replace the directory in there, since that's just the directory that I use. Also the .cpp is a short program that I wrote that does the exact same thing, just slower :)
--- start.sh
+++ start.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-if ! ls ~/StartupScripts/BkpPkgList/ | grep -q orig.txt; then
-	pacman -Q | cut -d" " -f1 > ~/StartupScripts/BkpPkgList/orig.txt
+export DIR="$HOME/StartupScripts/BkpPkgList"
+
+if ! ls $DIR/ | grep -q orig.txt; then
+	pacman -Q | cut -d" " -f1 > $DIR/orig.txt
 else
-	pacman -Q | cut -d" " -f1 | grep -xvf ~/StartupScripts/BkpPkgList/orig.txt > ~/StartupScripts/BkpPkgList/new.txt
-	sort -s -m -o ~/StartupScripts/BkpPkgList/tmp.txt ~/StartupScripts/BkpPkgList/orig.txt ~/StartupScripts/BkpPkgList/new.txt
-	mv ~/StartupScripts/BkpPkgList/tmp.txt ~/StartupScripts/BkpPkgList/orig.txt && rm ~/StartupScripts/BkpPkgList/new.txt
+	pacman -Q | cut -d" " -f1 | grep -xvf $DIR/orig.txt > $DIR/new.txt
+	sort -s -m -o $DIR/tmp.txt $DIR/orig.txt $DIR/new.txt
+	mv $DIR/tmp.txt $DIR/orig.txt && rm $DIR/new.txt
 fi
